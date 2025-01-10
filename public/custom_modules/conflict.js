0cdd93f3452458d34d8541493576631982ebad34
liwhong <liwhong@yonyou.com>
2025-01-10 16:40:14 +0800

    feat: init commit

---
+++ public/custom_modules/conflict.js
@@ -0,0 +1,7 @@
+/**
+ * @author  coderlwh
+ * @createDate  2025/1/10 15:00
+ * @description anythings
+ * @version 1.0
+ */
+const _ = 1111;
---
+++ public/custom_modules/minus.js
@@ -0,0 +1,9 @@
+/**
+ * @author  coderlwh
+ * @createDate  2025/1/10 14:58
+ * @description anythings
+ * @version 1.0
+ */
+export function minus(a, b){
+    return a - b;
+}
